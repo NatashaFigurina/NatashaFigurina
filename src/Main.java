dbb1fe027e56b614c1d7b1ca456c4c79e09b8265
natasha <figurinanatasha@gmail.com>
2024-05-10 14:36:32 +0300

    lesson_8. Добавлено решение задания.

---
+++ src/Main.java
@@ -0,0 +1,19 @@
+package src;
+
+public class Main {
+    public static void main(String[] args) {
+        AcceptStringArray stringArray = new AcceptStringArray();
+        int summa;
+        String[][] array = AcceptStringArray.addArray();
+        try {
+            summa = stringArray.workArray(array);
+        } catch (MyArraySizeException e) {
+            System.out.printf("Нарушена размерность массива! %s", e.getMessage());
+            return;
+        } catch (MyArrayDataException exc) {
+            System.out.printf("Ошибка: %s%n", exc.getMessage());
+            return;
+        }
+        System.out.println("Сумма элементов массива = " + summa);
+    }
+}
---
+++ src/MyArraySizeException.java
@@ -0,0 +1,7 @@
+package src;
+
+public class MyArraySizeException extends ArrayIndexOutOfBoundsException {
+    public MyArraySizeException(String massage) {
+        super(massage);
+    }
+}
