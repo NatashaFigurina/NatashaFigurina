2b9289e0df6296ca5ba17f85655332546f03f32e
natasha <figurinanatasha@gmail.com>
2024-05-18 09:13:08 +0300

    удалила лишний код

--- src/MyArrayDataException.java
+++ src/MyArrayDataException.java
@@ -1,5 +1,3 @@
-package src;
-
 public class MyArrayDataException extends NumberFormatException {
     public MyArrayDataException(int line, int column) {
         super(String.format("Неверные данные находятся в ячейке [%d, %d]\n", line, column));
--- src/MyArraySizeException.java
+++ src/MyArraySizeException.java
@@ -1,5 +1,3 @@
-package src;
-
 public class MyArraySizeException extends ArrayIndexOutOfBoundsException {
     public MyArraySizeException(String massage) {
         super(massage);
